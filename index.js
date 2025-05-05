8d05ad2277219abdd6f1e90a3e93e6d5586ecfb2
kai2503 <kai4you@icoud.com>
2025-05-05 14:34:07 +0200

    Änderungen an install_dependencies

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 // Middleware zum Parsen von JSON-Daten
 app.use(express.json());
 
 // Beispielroute
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.json({ staus: 200, message: 'Hello World!' });
 });
 
 // Server starten
